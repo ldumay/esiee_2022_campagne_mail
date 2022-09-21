8af74976eda6864bb864fd1fe2f2a8994a883730
CHEN <91790376+CHENZhihao98@users.noreply.github.com>
2022-09-21 15:04:34 +0200

    VS weird change

--- ESIEE_2_Campagne_Mail/ESIEE_2_Campagne_Mail/EnvoiCampagne.cs
+++ ESIEE_2_Campagne_Mail/ESIEE_2_Campagne_Mail/EnvoiCampagne.cs
@@ -16,5 +16,15 @@ namespace ESIEE_2_Campagne_Mail
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- ESIEE_2_Campagne_Mail/ESIEE_2_Campagne_Mail/Home.Designer.cs
+++ ESIEE_2_Campagne_Mail/ESIEE_2_Campagne_Mail/Home.Designer.cs
@@ -28,12 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Email = new System.Windows.Forms.Button();
+            this.Message = new System.Windows.Forms.Button();
+            this.Send_campaign = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Email
+            // 
+            this.Email.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Email.Location = new System.Drawing.Point(51, 149);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(185, 62);
+            this.Email.TabIndex = 0;
+            this.Email.Text = "Email";
+            this.Email.UseVisualStyleBackColor = true;
+            // 
+            // Message
+            // 
+            this.Message.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Message.Location = new System.Drawing.Point(295, 149);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(185, 62);
+            this.Message.TabIndex = 1;
+            this.Message.Text = "Message";
+            this.Message.UseVisualStyleBackColor = true;
+            this.Message.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Send_campaign
+            // 
+            this.Send_campaign.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Send_campaign.Location = new System.Drawing.Point(539, 149);
+            this.Send_campaign.Name = "Send_campaign";
+            this.Send_campaign.Size = new System.Drawing.Size(185, 62);
+            this.Send_campaign.TabIndex = 2;
+            this.Send_campaign.Text = "Send_campaign";
+            this.Send_campaign.UseVisualStyleBackColor = true;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Send_campaign);
+            this.Controls.Add(this.Message);
+            this.Controls.Add(this.Email);
+            this.Name = "Home";
             this.Text = "Home";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button Email;
+        private Button Message;
+        private Button Send_campaign;
     }
 }
