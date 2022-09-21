0ac67f4b81e9d73640f229514b6cf60b00f7a971
CHEN <91790376+CHENZhihao98@users.noreply.github.com>
2022-09-21 17:14:24 +0200

    Change bordel merge

--- ESIEE_2_Campagne_Mail/ESIEE_2_Campagne_Mail/CreerCampagne.Designer.cs
+++ ESIEE_2_Campagne_Mail/ESIEE_2_Campagne_Mail/CreerCampagne.Designer.cs
@@ -32,6 +32,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.Send_campaign = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox3
@@ -72,11 +73,21 @@
             this.Send_campaign.UseVisualStyleBackColor = true;
             this.Send_campaign.Click += new System.EventHandler(this.Send_campaign_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(205, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 15);
+            this.label2.TabIndex = 10;
+            // 
             // CreerCampagne
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.Send_campaign);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox3);
@@ -94,5 +105,6 @@
         private Label label3;
         private Label label1;
         private Button Send_campaign;
+        private Label label2;
     }
 }
