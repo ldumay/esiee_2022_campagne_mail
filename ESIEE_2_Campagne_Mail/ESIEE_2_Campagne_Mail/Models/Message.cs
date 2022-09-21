764fd6288031e49cd4301ab1a288bf929516c552
LDumay <dumay.famille@gmail.com>
2022-09-21 16:35:16 +0200

    Models ok

---
+++ ESIEE_2_Campagne_Mail/ESIEE_2_Campagne_Mail/Models/Message.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESIEE_2_Campagne_Mail.Models
+{
+    /**
+    * Classe Message.
+    */
+    internal class Message
+    {
+
+        //Attribut de la liste des mails
+        private string expediteur;
+        private string titre;
+        private string rebound;
+        private string contenu;
+
+        //Getters & Setter
+        public string Expediteur { get; set; }
+        public string Titre { get; set; }
+        public string Rebound { get; set; }
+        public string Contenu { get; set; }
+
+        /**
+         * Constructeur vide de la classe Message.
+         */
+        public Message()
+        {
+            expediteur = "";
+            titre = "";
+            rebound = "";
+            contenu = "";
+        }
+
+        /**
+         * Constructeur complet de la classe Message.
+         */
+        public Message(string expediteur, string titre, string rebound, string contenu)
+        {
+            this.expediteur = expediteur;
+            this.titre = titre;
+            this.rebound = rebound;
+            this.contenu = contenu;
+        }
+    }
+}
