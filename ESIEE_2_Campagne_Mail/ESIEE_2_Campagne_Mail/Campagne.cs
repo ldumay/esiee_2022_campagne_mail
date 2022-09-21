76eca236454361806cf8bae7a368abf6a87747a3
LDumay <dumay.famille@gmail.com>
2022-09-21 15:57:02 +0200

    Création des classes Model

---
+++ ESIEE_2_Campagne_Mail/ESIEE_2_Campagne_Mail/Campagne.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESIEE_2_Campagne_Mail
+{
+    internal class Campagne
+    {
+
+        //Attribut de la liste des mails
+        private List<GroupeMail> groupeMailList;
+        //Attribut du message
+        private Message message;
+
+        //Getters & Setter
+        public Message Message{get; set;}
+        public List<GroupeMail> GroupeMail { get; set;}
+
+        /**
+         * Constructeur vide de la classe Campagne.
+         */
+        public Campagne(){
+            this.groupeMailList = new List<GroupeMail>();
+            this.message = new Message();
+        }
+
+    }
+}
