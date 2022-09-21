0c9e9dc49800fe52ed3d24475966f5b51ac5bc32
CHEN <91790376+CHENZhihao98@users.noreply.github.com>
2022-09-21 15:02:48 +0200

    Add form page
    Créer campagne, Edit message, Envoi campagne et home

--- ESIEE_2_Campagne_Mail/ESIEE_2_Campagne_Mail/EnvoiCampagne.Designer.cs
+++ ESIEE_2_Campagne_Mail/ESIEE_2_Campagne_Mail/EnvoiCampagne.Designer.cs
@@ -28,19 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.Label label1;
+            this.Send_campaign = new System.Windows.Forms.Button();
+            this.sendingTest = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // Send_campaign
+            // 
+            this.Send_campaign.Location = new System.Drawing.Point(324, 221);
+            this.Send_campaign.Name = "Send_campaign";
+            this.Send_campaign.Size = new System.Drawing.Size(156, 57);
+            this.Send_campaign.TabIndex = 0;
+            this.Send_campaign.Text = "Send_campaign";
+            this.Send_campaign.UseVisualStyleBackColor = true;
+            this.Send_campaign.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sendingTest
+            // 
+            this.sendingTest.Location = new System.Drawing.Point(573, 159);
+            this.sendingTest.Name = "sendingTest";
+            this.sendingTest.Size = new System.Drawing.Size(75, 23);
+            this.sendingTest.TabIndex = 1;
+            this.sendingTest.Text = "Sending test";
+            this.sendingTest.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(252, 160);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(294, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            label1.Location = new System.Drawing.Point(171, 163);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(75, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Sending test";
+            label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // EnvoiCampagne
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.sendingTest);
+            this.Controls.Add(this.Send_campaign);
             this.Name = "EnvoiCampagne";
             this.Text = "EnvoiCampagne";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Button Send_campaign;
+        private Button sendingTest;
+        private TextBox textBox1;
+        private Label label1;
     }
 }
