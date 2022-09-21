751bc44a40cf3bf135e30c66e6400d5fc8ace63b
LDumay <dumay.famille@gmail.com>
2022-09-21 12:42:18 +0200

    Préparation des pages

---
+++ ESIEE_2_Campagne_Mail/ESIEE_2_Campagne_Mail/CreerCampagne.cs
@@ -0,0 +1,15 @@
+namespace ESIEE_2_Campagne_Mail
+{
+    public partial class CreerCampagne : Form
+    {
+        public CreerCampagne()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ESIEE_2_Campagne_Mail/ESIEE_2_Campagne_Mail/EditionMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ESIEE_2_Campagne_Mail
+{
+    public partial class EditionMessage : Form
+    {
+        public EditionMessage()
+        {
+            InitializeComponent();
+        }
+    }
+}
