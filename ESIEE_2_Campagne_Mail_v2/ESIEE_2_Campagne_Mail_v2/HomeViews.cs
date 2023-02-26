e76eec1a1c21460c4715825ee1533b58584d4e6a
ldumay <dumay.famille@gmail.com>
2023-02-26 14:46:48 +0100

    Petites corrections / intégration du code couleurs au code

--- ESIEE_2_Campagne_Mail_v2/ESIEE_2_Campagne_Mail_v2/HomeViews.cs
+++ ESIEE_2_Campagne_Mail_v2/ESIEE_2_Campagne_Mail_v2/HomeViews.cs
@@ -1,12 +1,16 @@
+using FontAwesome.Sharp;
+
 namespace ESIEE_2_Campagne_Mail_v2
 {
     public partial class HomeViews : Form
     {
+        /// <summary>
+        /// Constructeur
+        /// </summary>
         public HomeViews()
         {
             // Chargement des composants
             InitializeComponent();
-            
             // Chargement de la configuration de démarrage
             startConfiguration();
         }
@@ -16,15 +20,15 @@ namespace ESIEE_2_Campagne_Mail_v2
         /// </summary>
         private void startConfiguration()
         {
-            // Définissez le style de bordure du formulaire à une boîte de dialogue.
+            // Définit le style de bordure du formulaire à une boîte de dialogue.
             this.FormBorderStyle = FormBorderStyle.FixedDialog;
-            // Maximizer la boîte de maximisation.
-            this.MaximizeBox = false;
-            // Supprimer la boîte de réduction.
+            // Maximise la boîte de maximisation.
+            //this.MaximizeBox = false;
+            // Supprime la boîte de réduction.
             this.MinimizeBox = false;
-            // Définir la position de départ du formulaire au centre de l'écran.
+            // Définit la position de départ du formulaire au centre de l'écran.
             this.StartPosition = FormStartPosition.CenterScreen;
-            // Affichez le formulaire sous la forme d'une boîte de dialogue modale.
+            // Affiche le formulaire sous la forme d'une boîte de dialogue modale.
             //this.ShowDialog();
         }
     }
